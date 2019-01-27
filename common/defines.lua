87742b7f9206d1f6b7a516295ee6c96bcedc3af0
Foffy123 <foffy123@gmail.com>
2019-01-27 15:34:29 -0800

    Minimum ruler skill is now 1-1-1

--- common/defines.lua
+++ common/defines.lua
@@ -409,7 +409,7 @@ NCountry = {
 	CORRUPTION_FROM_BANNERS = 5,
 
 
-	MONARCH_MIN_SKILL = 0,						-- Probably bad idea to reduce this
+	MONARCH_MIN_SKILL = 1,						-- Probably bad idea to reduce this
 	MONARCH_MAX_SKILL = 6,						-- Monarch skills will be chosen between MONARCH_MIN_SKILL and MONARCH_MAX_SKILL
 	ELECTIVE_LOCAL_MONARCH_MIN_BONUS = 0,		-- The minimum change that is applied to non-foreign heirs in elective monarchy
 	ELECTIVE_LOCAL_MONARCH_MAX_BONUS = 1,		-- The maximum change that is applied to non-foreign heirs in elective monarchy
