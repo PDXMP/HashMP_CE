3c084b107d0d1358f1862e1cff0de87b12330f27
Pieman90 <71159224+Pieman90@users.noreply.github.com>
2021-05-06 16:23:27 -0400

    Add files via upload

--- common/defines.lua
+++ common/defines.lua
@@ -18,7 +18,7 @@ NGame = {
 	MAX_CUSTOM_COUNTRIES = 75,				-- Max is 100
 	MAX_OBSERVERS = 10,						-- Max is 100
 
-	SIZE_LIMIT_FOR_ISLANDS = 25000,			-- Threshold in pixels to define what is an island and what is a continent
+	SIZE_LIMIT_FOR_ISLANDS = 45000,			-- Threshold in pixels to define what is an island and what is a continent
 
 	DAYS_BEHIND_PAUSE = 75,					-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will pause
 	DAYS_BEHIND_LOWER_SPEED = 30,			-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will slow down
